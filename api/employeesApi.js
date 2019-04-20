e0513b847eab3b32fd25cc4160c282dbe2079bf8
ntarunmenon <ntarunmenon@gmail.com>
2019-04-21 06:38:31 +1000

    Post Employee

--- api/employeesApi.js
+++ api/employeesApi.js
@@ -4,6 +4,7 @@ const uuidv1 = require('uuid/v1');
 const moment = require('moment')
 var router = express.Router()
 const { getEmployees,updateEmployee, deleteEmployee } = require("./../file/employeesFile");
+const { getLocations } = require("./../file/locationFile");
 
 router.get('/', (req, res) => {
      getEmployees()
@@ -13,8 +14,20 @@ router.get('/', (req, res) => {
 router.post('/', (req, res) => {
     var newEmployee = req.body;
     newEmployee.mntEmpId = uuidv1()
+    newEmployee.isActive = "Y"
+    newEmployee.password = "pass1234"
+    newEmployee.enabled = true
+    newEmployee.accountNonLocked = true
+    newEmployee.accountNonExpired = true
+    newEmployee.credentialsNonExpired = true
     newEmployee.createDate = moment().format('YYYY-MM-DD')
     newEmployee.modifiedDate = moment().format('YYYY-MM-DD')
+    newEmployee.srvEmpRoleList = []
+    for (employeePrivelege of newEmployee.employeePriveleges) {
+        newEmployee.srvEmpRoleList.push(_.cloneDeep(employeePrivelege))
+        newEmployee.srvEmpRoleList.empRoleId = uuidv1()
+    }
+    newEmployee.empLocationId = getLocations().then(locations => {return locations[0]})
     updateEmployee(newEmployee)
    .then(res.send(newEmployee.mntEmpId))
 })
--- index.js
+++ index.js
@@ -9,10 +9,10 @@ const app = express()
 const port = 3000
 
 app.use(bodyParser.json())
-app.use('/auditLogs', auditLog)
-app.use('/employees', employees)
-app.use('/locations', locations)
-app.use('/login', login)
+app.use('/api/auditlogs', auditLog)
+app.use('/api/employees', employees)
+app.use('/api/locations', locations)
+app.use('/api/login', login)
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
